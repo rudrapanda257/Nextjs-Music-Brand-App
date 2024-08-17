1bc6418b6ae4c96ab06f314e842132a72385b414
Rudra Narayan Panda <rudrapanda357@gmail.com>
2024-08-17 12:35:16 +0530

    footer change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
-import Footer from "@/components/footer";
+import Footer from "@/components/Footer";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
